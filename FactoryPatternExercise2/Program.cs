8d49307490004aea7513e10984745611bb572197
Bailey Hart <baileyhart100@gmail.com>
2023-06-13 14:04:16 -0400

    Initial Commit

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            bool correctInput;
+
+            do
+            {
+                Console.Clear();
+                correctInput = true;
+
+                Console.WriteLine("What database would you like to work with?");
+                Console.WriteLine($"Type: sql");
+                Console.WriteLine($"Type: mongo");
+                Console.WriteLine($"Type: list");
+
+
+                userInput = Console.ReadLine();
+
+
+                if(userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput=false;
+                    Console.WriteLine($"Please pick one of the three database options");
+                    Thread.Sleep(1500);
+                }
+
+
+            } while (!correctInput); ;
+
+            Console.Clear();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var Products = db.LoadData();
+            db.SaveData();
+
+            foreach (var product in Products)
+            {
+                Console.WriteLine($"Name: {product.Name}");
+                Console.WriteLine($"Price: {product.Price}");
+            }
         }
     }
 }
